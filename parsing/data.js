bcf884e2cdca12331014950a82066dbc50458ec5
David W. Farmer <farmer@aimath.org>
2025-04-04 23:18:24 -0700

    data file: specifying the language

---
+++ parsing/data.js
@@ -0,0 +1,30 @@
+
+// this list is not used, it serves to help keep track of tags requiring special attention
+randomtags = ["fn", "footnote", "title", "statement", "reading-questions",
+              "figure", "image", "introduction", "output", "blockquote",
+              "definition", "exercisegroup", "mrow", "sidebyside", "worksheet",
+              "table"];
+
+// LaTeX, TeX, PreTeXt, [[what else?]]
+
+list-like = ["ol", "ul", "dl];
+
+aside-like = ["aside", "historical", "biographical"];
+
+theorem-like = ["algorithm", "claim", "corollary", "fact", "identity", "lemma", "proposition", "theorem"];
+
+// can definition be axiom-like?
+axiom-like = ["assumption", "axiom", "conjecture", "heuristic", "hypothesis", "principle"]
+
+remark-like = ["convention", "insight", "note", "observation", "remark", "warning"];
+
+example-like = ["example", "problem", "question"]
+
+definition-like = ["definition", "axiom"]
+
+hint-like = ["hint", "answer", "solution"];
+
+project-like = ["activity", "exploration", "investigation", "project"]
+
+exercise-like = ["exercise'];
+
