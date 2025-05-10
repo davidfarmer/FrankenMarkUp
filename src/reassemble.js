78de6ab604e7347ea0229f853743af171110c435
Oscar Levin <oscar.levin@unco.edu>
2025-05-10 11:08:20 -0600

    reset space_math import to pull from github repo

--- src/reassemble.js
+++ src/reassemble.js
@@ -1,7 +1,7 @@
 
 // import { outputtags, debugging_output_markup, PTXdisplayoutput } from "./parse-exports";
 import { debugging_output_markup, PTXdisplayoutput, outputtags } from "./parse";
-import { convertMathSnippet } from '../../Space_Math/src/main.js';
+import { convertMathSnippet } from 'space_math';
 
 let debugtags = "STart";
 debugtags = "";
